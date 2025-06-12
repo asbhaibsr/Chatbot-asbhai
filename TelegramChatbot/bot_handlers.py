77241717fb1ce40bf82bb6b6da860c0daf20cfd9
asbhaibsr <ffloverisarsad@gmail.com>
2025-06-13 01:01:45 +0530

    Update bot_handlers.py

--- TelegramChatbot/bot_handlers.py
+++ TelegramChatbot/bot_handlers.py
@@ -1,1 +1,9 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("👋 Hello! Main ek stylish Ladki hoon 🤭\nBot chalu ho gaya!")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Yeh lo meri madad: /start /help")
 
