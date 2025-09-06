c11d56189ab980a44ef11df118e4d9f8a809de45
Asbhaibsr <ffloverisarsad@gmail.com>
2025-09-06 20:20:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,20 @@
 import threading
 import logging
 from pyrogram import idle
+import nltk # NLTK library import kiya
+import os # OS library import kiya
+
+# NLTK data download check
+# Agar 'vader_lexicon' pehle se downloaded nahi hai, toh download karega.
+# Yeh step aapke bot ko kisi bhi naye environment mein chalne ke liye aasan banata hai.
+try:
+    nltk.data.find('sentiment/vader_lexicon.zip')
+except nltk.downloader.DownloadError:
+    print("vader_lexicon not found, downloading now...")
+    # NLTK data ke liye ek path set karein taaki woh usko sahi jagah par store kare.
+    nltk.download('vader_lexicon', download_dir='/usr/share/nltk_data')
+    os.environ['NLTK_DATA'] = '/usr/share/nltk_data'
+    print("Download complete.")
 
 # Import necessary components from other files
 from config import app, logger, flask_app
